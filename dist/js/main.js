bd4af135ca7806e63fad7870fbb68807fcbdb81c
Georgy <marakhovskij@gmail.com>
2021-04-23 14:21:39 +0300

    Калькурятор калорий

--- dist/js/main.js
+++ dist/js/main.js
@@ -385,6 +385,82 @@ dotsArray.forEach(dot => {
 });
 
 
+// Калькулятор калорий
+
+
+const result = document.querySelector('.calculating__result span');
+let sex = 'female',
+    height,
+    weight,
+    age,
+    ratio = 1.375;
+
+function calcTotal() {
+  if (!sex || !height || !weight || !age || !ratio) {
+    result.textContent = '____';
+    return;
+  }
+
+  if (sex == 'female') {
+    result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+  } else {
+    result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+  }
+}
+calcTotal();
+
+function getStaticInfo(parent, activeClass) {
+  const elements = document.querySelectorAll(`${parent} div`);
+
+  elements.forEach(elem => {
+    elem.addEventListener('click', (evt) => {
+      if (evt.target.getAttribute('data-ratio')) {
+        ratio = +evt.target.getAttribute('data-ratio');
+      } else {
+        sex = evt.target.getAttribute('id');
+      }
+
+      elements.forEach(elem => {
+        elem.classList.remove(activeClass);
+      });
+
+      evt.target.classList.add(activeClass);
+
+      calcTotal();
+    });
+  });
+}
+getStaticInfo('#gender', 'calculating__choose-item_active');
+getStaticInfo('.calculating__choose_big', 'calculating__choose-item_active');
+
+function getDynamicInfo(selector) {
+  const input = document.querySelector(selector);
+
+  input.addEventListener('input', () => {
+    switch (input.getAttribute('id')) {
+      case 'height':
+        height = +input.value;
+        break;
+      case 'weight':
+        weight = +input.value;
+        break;
+      case 'age':
+        age = +input.value;
+        break;
+    }
+    calcTotal();
+  });
+}
+getDynamicInfo('#height');
+getDynamicInfo('#weight');
+getDynamicInfo('#age');
+
+
+
+
+
+
+
 // Простой слайдер
 
 
